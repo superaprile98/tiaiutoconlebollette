9a933254a418a22e0ac77241686c9e18d8cb4c97
superaprile98 <filippoaprile98@gmail.com>
2026-02-28 00:22:41 +0100

    fix form contatto

--- src/pages/api/send-email.ts
+++ src/pages/api/send-email.ts
@@ -5,9 +5,10 @@ export const POST: APIRoute = async ({ request, locals }) => {
   const data = await request.formData();
   const nome = data.get("name")?.toString();
   const mail = data.get("contact")?.toString();
+  const subject = data.get("subject")?.toString();
   const messaggio = data.get("message")?.toString();
 
-  if (!nome || !mail || !messaggio) {
+  if (!nome || !mail || !subject || !messaggio) {
     return new Response(
       JSON.stringify({
         message: "Campi mancanti.",
@@ -33,12 +34,16 @@ export const POST: APIRoute = async ({ request, locals }) => {
 
   try {
     const { error } = await resend.emails.send({
-      from: "onboarding@resend.dev",
-      to: ["baugigi@proton.me"],
-      replyTo: mail, // Resend lo accetta, ma deve essere in un formato verosimile "email@email.com" - il from name si usa <Name> email@email.com, non per il replyTo
-      subject: `Nuovo mail dal sito web da: ${nome}`,
-      text: `Nuovo Messaggio dal sito web.\n\nNome: ${nome}\nmail (Tel o Email): ${mail}\n\nMessaggio:\n${messaggio}`,
-    });
+  // 1. Cambia il mittente! Non usare più onboarding@resend.dev
+  from: "Ti aiuto con le bollette <contatti@tiaiutoconlebollette.it>", 
+  
+  // 2. Ora questo funzionerà senza problemi
+  to: ["info@tiaiutoconlebollette.it"], 
+  
+  replyTo: mail,
+  subject: `Richiesta ${subject} da ${mail}`,
+  text: `${messaggio}\n\n---\nNome: ${nome}\nMail: ${mail}`,
+});
 
     if (error) {
       return new Response(
