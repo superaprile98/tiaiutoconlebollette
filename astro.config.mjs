b4166c7b574a8e50bc46af7a058b6909a3bc018c
superaprile98 <filippoaprile98@gmail.com>
2026-03-02 13:44:26 +0100

    aggiunti robots.txt e sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,14 @@ import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import cloudflare from "@astrojs/cloudflare";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
+  site: "https://www.tiaiutoconlebollette.it",
   output: "server",
   adapter: cloudflare(),
+  integrations: [sitemap()],
   vite: {
     plugins: [tailwindcss()],
   },
